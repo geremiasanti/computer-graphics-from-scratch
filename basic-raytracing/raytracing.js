a8e56168498e31498d7d2c45f6f5ee34d5624678
geremia <germysettesette@gmail.com>
2024-04-18 19:30:47 +0200

    color

--- basic-raytracing/raytracing.js
+++ basic-raytracing/raytracing.js
@@ -1,12 +1,25 @@
+function rgb(r, g, b) {
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
 class Canvas {
     constructor(canvasElementId) {
         this.htmlElement = document.getElementById(canvasElementId);
+        this.context = this.htmlElement.getContext('2d');
+
         this.width = this.htmlElement.width;
         this.height = this.htmlElement.height;
-        this.context = this.htmlElement.getContext('2d');
+        this.minX = -this.width/2;
+        this.maxX = this.width/2;
+        this.minY = -this.height/2;
+        this.maxY = this.height/2;
     }
     
-    putPixel(x, y) {
+    putPixel(x, y, rgb) {
+        // color
+        this.context.fillStyle = rgb;
+
+        // position
         this.context.fillRect(
             this.width/2 + x,
             this.height/2 - y,
@@ -16,5 +29,20 @@ class Canvas {
     }
 }
 
-let canvas = new Canvas('canvas');
-canvas.putPixel(0, 0);
+function render() {
+    let canvas = new Canvas('canvas');
+    for(let x = canvas.minX; x <= canvas.maxX; x++) {
+        for(let y = canvas.minY; y <= canvas.maxY; y++) {
+            // quadrants
+            if(x == 0 || y == 0) 
+                canvas.putPixel(x, y, rgb(0, 0, 0));
+
+            // square in positive quadrant
+            if(0 < x && x < canvas.width/7 && 0 < y && y < canvas.height/7)
+                canvas.putPixel(x, y, rgb(255, 0, 0));
+        }
+    }
+}
+
+
+render();
